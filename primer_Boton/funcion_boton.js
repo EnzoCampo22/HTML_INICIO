c59c69c6af5d8ee98039d8c56cf7a0a7c0e9b102
EnzoCampo22 <enzocampo_1991@hotmail.com>
2023-01-19 14:22:20 -0300

    Haciendo mi primer boton en jscript

---
+++ primer_Boton/funcion_boton.js
@@ -0,0 +1,8 @@
+document.getElementById("boton_Magico").onclick = function(){
+    console.log("esto lo vemos nosotros nomas");
+
+
+    document.getElementById("demo").innerHTML = "aplicando javascript";
+
+
+}
